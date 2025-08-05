0bc7e7a04f4a7da14f78e89c42cca0738daca998
jdmchardy <90758975+jdmchardy@users.noreply.github.com>
2025-08-05 14:10:07 +0100

    Update funamori_XRD.py

--- funamori_XRD.py
+++ funamori_XRD.py
@@ -341,9 +341,9 @@ def cost_function(params, param_flags, selected_hkls, Gaussian_FWHM, phi_values,
     y_sim_common = interp_sim(x_exp_common)
 
     residuals = np.asarray(y_exp_common - y_sim_common)
-    st.write(np.len(residuals))
-    st.write(np.len(y_exp_common))
-    st.write(np.len(y_sim_common))
+    st.write(len(residuals))
+    st.write(len(y_exp_common))
+    st.write(len(y_sim_common))
     #weighted_residuals = residuals * (1 / (y_exp_common + 1))
     
     #return weighted_residuals
