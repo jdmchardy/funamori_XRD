d33f42f20c32a8abede8a76dee9d7d07b16626b7
jdmchardy <90758975+jdmchardy@users.noreply.github.com>
2025-08-06 18:14:44 +0100

    Update funamori_XRD.py

--- funamori_XRD.py
+++ funamori_XRD.py
@@ -489,8 +489,8 @@ def generate_posterior(fit_result, param_flags, selected_hkls, intensities, Gaus
             x_exp_common, y_exp_common, bin_indices
         )
     posterior = minimize(wrapped_cost_function, 
-                         method='emcee', nan_policy='omit', burn=5, steps=50, thin=1, nwalkers=10,
-                     params=fit_result.params, is_weighted=False, progress=False)
+                         method='emcee', nan_policy='omit', burn=20, steps=100, thin=1, nwalkers=20,
+                         params=fit_result.params, is_weighted=False, progress=False)
     return posterior
 
 #### Main App logic -----------------------------------------------------
