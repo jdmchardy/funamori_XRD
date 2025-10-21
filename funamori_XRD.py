08d5fb99164ffc6484a65e4e9c53f3f60aadca82
jdmchardy <90758975+jdmchardy@users.noreply.github.com>
2025-10-21 16:37:46 +0100

    Update funamori_XRD.py

--- funamori_XRD.py
+++ funamori_XRD.py
@@ -597,7 +597,7 @@ if uploaded_file:
     elif symmetry == "tetragonal_B":
         required_keys = {'a','b','c','alpha','beta','gamma','wavelength','C11','C33','C12','C13','C16','C44','C66','sig11','sig22','sig33'}
     else:
-        st.error(f"{} symmetry is not yet supported".format(symmetry))
+        st.error("{} symmetry is not yet supported".format(symmetry))
         required_keys = {}
     if not required_keys.issubset(metadata):
         st.error(f"CSV must contain: {', '.join(required_keys)}")
