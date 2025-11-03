0f61d3b350ca2904fc2989baa9a06542b5603b9a
jdmchardy <90758975+jdmchardy@users.noreply.github.com>
2025-11-03 18:27:33 +0000

    Update funamori_XRD.py

--- funamori_XRD.py
+++ funamori_XRD.py
@@ -386,7 +386,7 @@ def Generate_XRD(selected_hkls, intensities, Gaussian_FWHM, strain_sim_params, b
                 gaussian_peak = peak_intensity * Gaussian(twotheta_grid, two_theta, sigma_gauss) 
                 total_gauss += gaussian_peak
         else: #Run the code for mean positions (Singh pattern - one average peak per reflection)
-            two_theta = np.asarray(group["Mean two_th"].values[0])
+            two_theta = np.asarray([group["Mean two_th"].values[0]])
             gaussian_peak = peak_intensity * Gaussian(twotheta_grid, two_theta, sigma_gauss)
             total_gauss += gaussian_peak
             
