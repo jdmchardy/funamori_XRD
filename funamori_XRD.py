ba4fa48c7537815e631690ad113a3a116448070a
jdmchardy <90758975+jdmchardy@users.noreply.github.com>
2025-11-05 12:54:38 +0000

    Update funamori_XRD.py

--- funamori_XRD.py
+++ funamori_XRD.py
@@ -1439,26 +1439,26 @@ if uploaded_file is not None:
                     psi_values
                 )
                 st.rerun()
-                
-                # Display in Streamlit
-                st.markdown("### Fit Report")
-                st.code(report_str)
-                
-                XRD_df = Generate_XRD(selected_hkls, intensities_refined, Gaussian_FWHM, strain_sim_params, Funamori_broadening)
-                twoth_sim = XRD_df["2th"]
-                intensity_sim = XRD_df["Total Intensity"]
-                x_min_sim = np.min(twoth_sim)
-                x_max_sim = np.max(twoth_sim)
-                mask = (x_exp >= x_min_sim) & (x_exp <= x_max_sim)
-                x_exp_common = x_exp[mask]
-                y_exp_common = y_exp[mask]
-                interp_sim = interp1d(twoth_sim, intensity_sim, bounds_error=False, fill_value=np.nan)
-                y_sim_common = interp_sim(x_exp_common)
-    
-                plot_overlay(x_exp_common, y_exp_common, x_exp_common, y_sim_common, title="Refined Fit")
 
             else:
                 st.error("Refinement failed.")
+        if result.success:
+            # Display in Streamlit
+            st.markdown("### Fit Report")
+            st.code(report_str)
+            
+            XRD_df = Generate_XRD(selected_hkls, intensities_refined, Gaussian_FWHM, strain_sim_params, Funamori_broadening)
+            twoth_sim = XRD_df["2th"]
+            intensity_sim = XRD_df["Total Intensity"]
+            x_min_sim = np.min(twoth_sim)
+            x_max_sim = np.max(twoth_sim)
+            mask = (x_exp >= x_min_sim) & (x_exp <= x_max_sim)
+            x_exp_common = x_exp[mask]
+            y_exp_common = y_exp[mask]
+            interp_sim = interp1d(twoth_sim, intensity_sim, bounds_error=False, fill_value=np.nan)
+            y_sim_common = interp_sim(x_exp_common)
+
+            plot_overlay(x_exp_common, y_exp_common, x_exp_common, y_sim_common, title="Refined Fit")
 
         #Next display a button to compute the posterior probability distribution
         #st.subheader("Probe fit surface")
